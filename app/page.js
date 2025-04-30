e65871c6dbbcd519bdb2ade30f17e36f56838285
0xSunill <sunday7637@gmail.com>
2025-04-30 13:23:23 +0000

    install dependencies

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      hello
+    <div className="underline text-3xl">
+      hello sir
     </div>
   );
 }
